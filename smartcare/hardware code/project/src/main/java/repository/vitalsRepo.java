1e817fcff7843c11262f7a9d58fed80700590fd0
Simran-Baheti <64544471+Simran-Baheti@users.noreply.github.com>
2022-03-05 16:34:00 +0530

    Delete smartcare directory

--- smartcare/hardware code/project/src/main/java/repository/vitalsRepo.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import model.vitals;
-
-public interface vitalsRepo extends MongoRepository<vitals, String>{
-	
-}
